f89e0773dce99d3a7a6957ca6cd4e211a762427a
Matthaus Litteken <matthaus@puppetlabs.com>
2012-01-18 21:32:22 -0800

    Working well now. Need to do some framework work.

--- hw1.rb
+++ hw1.rb
@@ -1,8 +1,13 @@
 train         = ARGV[1] || "optdigits.training"
 test          = ARGV[2] || "optdigits.testing"
-learning_rate = ARGV[3] || 0.2
+learning_rate = ARGV[3] || 0.1
+number        = ARGV[4] || 8
 
 require 'Perceptron'
-
-p = Perceptron.new(train, test, learning_rate)
-p.train(10)
+(0..9).reject{|i| i == number}.each do | i |
+  puts "Run for #{number} vs. #{i}"
+  puts "=========================="
+  p = Perceptron.new(train, test, learning_rate, i, number).train
+  puts "End of run....."
+  puts
+end
--- lib/perceptron.rb
+++ lib/perceptron.rb
@@ -1,51 +1,52 @@
 # Basic Perceptron using Stochastic Descent
 class Perceptron
   # List of instance variables that need accessor methods created
-  attr_accessor :weights, :learning_rate, :training_set, :testing_set, :epochs
+  attr_accessor :weights, :learning_rate, :training_set, :testing_set, :epochs, :num
 
   # Constructor for the Perceptron, takes training file and testing file and learning rate as arguments
-  def initialize(train_file, test_file, learning_rate)
+  def initialize(train_file, test_file, learning_rate, num1, num2)
+    @learning_rate = learning_rate
     random = Random.new(1234)
 
-    # Arrays of 10 for each of the 10 digits
-    @training_set = Array.new(10).map! { | i | Array.new }
-    @testing_set = Array.new(10).map!  { | i | Array.new }
+    # Variable to hold the training and testing data
+    @training_set = Array.new(0)
+    @testing_set = Array.new(0)
 
     # Populate the arrays
     load_data(train_file, @training_set)
     load_data(test_file, @testing_set)
 
     # Num nodes (number of attributes plus the bias node)
-    num_nodes = @training_set.first.first.size+1
+    num_nodes = @training_set.first.size-1
 
     # Initialize weights
     @weights = Array.new(num_nodes)
     @weights.collect! { | i | random.rand(-1.0..1.0) }
 
     # Initialize epochs to 0
-    @epochs = 0
+    @epochs = 1
+    @num1 = num1
+    @num2 = num2
   end
 
   private
 
   # Load data from files into the array
   def load_data(file, array)
-    count = 0
     File.open(file).readlines.each do | line |
-      count += 1
-      cur_line = line.chomp!.split(',')
-      index = cur_line.pop.to_i
-      if array[index].nil?
-        array[index] = Array.new(1, cur_line)
-      else
-        array[index] << cur_line
-      end
+      cur_line = line.chomp!.split(',').unshift("1")
+      #puts "Wrong size in file." && exit unless cur_line.size == 66
+      cur_line.map! { | elem | elem.to_i }
+      array << cur_line
     end
   end
 
   # After each training element is processed, the weights should be updated
-  def update_weights
-
+  def update_weights(elems, target, output)
+    adjustment = @learning_rate * (target - output)
+    @weights.map!.each_with_index do | weight, i |
+      weight += adjustment * elems[i]
+    end
   end
 
 public
@@ -56,16 +57,59 @@ public
 
   # Training takes an optional duration argument for # of epochs to train.
   def train (duration = nil)
-    #unless duration.nil?
-    0.upto(duration) do | i |
-      (@training_set[0]+@training_set[8]).each do | x |
-        sum = 0
-        x.each_with_index do | y, i |
-          sum += (y * @weights[i])
-        end
-        
+    # Trying to recognize 8, so 8 is 1, other is -1
+    if duration.nil?
+      while do_train(@epochs) != 100
+        @epochs += 1
+      end
+    else
+      1.upto(duration) do | i |
+        do_train(i)
       end
     end
+  end
+
+  # This abstracts the meat of train so train can operate 
+  # on a set # of epochs or until 100% is reached
+  def do_train(i)
+    puts "Epoch ##{i}"
+    puts "============="
+    count = 0
+    correct = 0
+
+    # Calculate the activation
+    @training_set.each do | example |
+      # Set the target
+      answer = example.last
+      next unless (answer == @num1 || answer == @num2)
+      target = (answer == @num1 ? 1 : -1)
+
+      # Initialize sum to 0
+      sum = 0
+      example.each_with_index do | elem, j |
+        next if j == example.size-1
+        sum += (elem * @weights[j])
+      end
+
+      output = ( sum <= 0 ? -1 : 1 )
+
+      # Update the weights if appropriate
+      if output == target then
+        correct += 1
+      else
+        update_weights(example, target, output)
+      end
+      count += 1
+    end
+
+    accuracy = correct.to_f/count.to_f * 100
+
+    puts "Correct: #{correct}"
+    puts "Incorrect: #{count - correct}"
+    puts "Accuracy: #{accuracy}%"
+    puts
+
+    accuracy
   end
 
 end
